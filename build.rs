d5f2c33d7a0d194105f8af75b6334bdb15e7c89a
Ayrzo <ayrzo@lurito.cn>
2024-11-16 01:09:56 +0800

    chore(build): Updated `build.rs` to support compilation on Windows
    BREAKING CHANGE: **Zig Compiler is required now**

--- build.rs
+++ build.rs
@@ -4,32 +4,90 @@ use std::process::Command;
 
 fn main() {
     // Navigate to the C library directory
-    let crate_dir: String = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let lib_dir = format!("{}/depends/yespower", crate_dir);
-    let gcc = "gcc";
+    let crate_dir = &env::var("CARGO_MANIFEST_DIR").unwrap();
+    let lib_dir = &format!("{}/depends/yespower", crate_dir);
+    let lib_out_dir = &format!("{}/target/depends/{}/yespower", crate_dir, env::var("TARGET").unwrap());
 
-    // Compile the necessary components
-    Command::new(gcc)
-        .current_dir(&lib_dir)
-        .args(&["-c", "-O2", "-fomit-frame-pointer", "-funroll-loops", "yespower-opt.c", "sha256.c"])
-        .status()
-        .unwrap();
+    // Compile the library
+    // Use a code block to avoid variable name pollution
+    {
+        let obj_ext_name = if cfg!(target_os = "windows") { "obj" } else { "o" };
 
-    // Link the object files into a static library
-    Command::new("ar")
-        .current_dir(&lib_dir)
-        .args(&["rcs", "libyespower.a", "yespower-opt.o", "sha256.o"])
-        .status()
-        .unwrap();
+        // Create the output directory if it doesn't exist
+        let lib_out_dir_path = PathBuf::from(lib_out_dir);
+        if !lib_out_dir_path.exists() {
+            let _ = std::fs::create_dir_all(&lib_out_dir_path);
+        }
+
+        fn build_lib_to_obj(name: &str, crate_dir: &str, lib_dir: &str, lib_out_dir: &str, obj_ext_name: &str) {
+            let output_name = &format!("{}/{}.{}", lib_out_dir, name, obj_ext_name);
+
+            // Return if output file exists
+            if PathBuf::from(output_name).exists() {
+                return;
+            }
+
+            // Compile
+            let status = Command::new("zig")
+                .current_dir(crate_dir)
+                .args([
+                    "cc",
+                    "-c",
+                    "-O3",
+                    "-fomit-frame-pointer",
+                    "-funroll-loops",
+                    &format!("{}/{}.c", lib_dir, name),
+                    "-o",
+                    output_name,
+                ])
+                .status()
+                .expect("failed to execute process");
+
+            if !status.success() {
+                eprintln!("Failed to compile {}.c", name);
+                std::process::exit(1);
+            }
+        }
+
+        // Compile the necessary components
+        build_lib_to_obj("yespower-opt", crate_dir, lib_dir, lib_out_dir, obj_ext_name);
+        build_lib_to_obj("sha256", crate_dir, lib_dir, lib_out_dir, obj_ext_name);
+
+        let output_lib_name = &match env::consts::OS {
+            "linux" => format!("{}/libyespower.a", lib_out_dir),
+            "windows" => format!("{}/yespower.lib", lib_out_dir),
+            _ => panic!("Unsupported operating system"),
+        };
+
+        // Only link a library if lib file doesn't exist
+        if !PathBuf::from(output_lib_name).exists() {
+            let status = Command::new("zig")
+                .current_dir(lib_dir)
+                .args([
+                    "ar",
+                    "rcs",
+                    output_lib_name,
+                    &format!("{}/{}.{}", lib_out_dir, "yespower-opt", obj_ext_name),
+                    &format!("{}/{}.{}", lib_out_dir, "sha256", obj_ext_name),
+                ])
+                .status()
+                .expect("failed to link libs");
+
+            if !status.success() {
+                eprintln!("Failed to create static library yespower.lib");
+                std::process::exit(1);
+            }
+        }
+    }
 
     // Link the compiled library
-    println!("cargo:rustc-link-search=native={}", lib_dir);
+    println!("cargo:rustc-link-search=native={}", lib_out_dir);
     println!("cargo:rustc-link-lib=static=yespower");
 
     // Generate bindings
     let bindings = bindgen::Builder::default()
         .header(format!("{}/yespower.h", lib_dir))
-        .clang_arg(format!("-I{}", lib_dir))
+        .clang_arg(format!("-I{}", lib_out_dir))
         .generate()
         .expect("Failed to generate bindings.");
 
